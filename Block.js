d7e0ab364fee0076e7e063dac1c6abad9c247324
amoazeni75 <amoazeni75@gmail.com>
2020-05-20 03:14:12 +0430

    -Create the first element of our project that is the Block Class -Create a simple dev-test script

---
+++ Block.js
@@ -0,0 +1,24 @@
+class Block {
+    constructor(timestamp, lastBlockHash, hash, data) {
+        /**
+         * timestamp: we need this for synchronization
+         * lastBlockHash: is the hash of previous block
+         * hash: it is the hash for the current block
+         * data: what we want to store within the block
+         */
+        this.timestamp = timestamp;
+        this.lastBlockHash = lastBlockHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        return `Block-
+            Timestamp: ${this.timestamp},
+            Last Block Hash: ${this.lastBlockHash.substring(0, 10)},
+            Current Block Hash: ${this.hash.substring(0, 10)},
+            Stored Data: ${this.data}`;
+    }
+}
+
+module.exports = Block;
---
+++ dev-test.js
@@ -0,0 +1,4 @@
+const Block = require('./Block')
+
+const block_instance = new Block('T', 'LH', 'H', 'D');
+console.log(block_instance.toString());
