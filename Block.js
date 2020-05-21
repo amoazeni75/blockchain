93673b3c042fae90041c3d330878b41a3c9c5047
amoazeni75 <amoazeni75@gmail.com>
2020-05-21 21:27:32 +0430

    -Hash function using SHA256 implemented

--- Block.js
+++ Block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastBlockHash, hash, data) {
         /**
@@ -30,8 +32,12 @@ class Block {
          */
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = 'todo-hash';
+        const hash = Block.hash(timestamp, lastHash, data);
         return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 
