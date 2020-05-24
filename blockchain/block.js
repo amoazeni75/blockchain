4525b0823c34a482a0b6cc0db6b06e2560ccb04d
amoazeni75 <amoazeni75@gmail.com>
2020-05-24 20:17:04 +0430

    -Proof-of-work and nonce added to the block and mine sections

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,7 +1,8 @@
 const SHA256 = require('crypto-js/sha256');
+const {DIFFICULTY} = require('../config');
 
 class Block {
-    constructor(timestamp, lastBlockHash, hash, data) {
+    constructor(timestamp, lastBlockHash, hash, data, nonce) {
         /**
          * timestamp: we need this for synchronization
          * lastBlockHash: is the hash of previous block
@@ -12,6 +13,7 @@ class Block {
         this.lastBlockHash = lastBlockHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
     }
 
     toString() {
@@ -19,30 +21,38 @@ class Block {
             Timestamp: ${this.timestamp},
             Last Block Hash: ${this.lastBlockHash.substring(0, 10)},
             Current Block Hash: ${this.hash.substring(0, 10)},
+            Nonce:${this.nonce},
             Stored Data: ${this.data}`;
     }
 
     static genesis() {
-        return new this("Genesis Time", "-----", "f1er56-s4odw", [])
+        return new this("Genesis Time", "-----", "f1er56-s4odw", [], 0)
     }
 
     static mineBlock(lastBlock, data) {
         /**
          * data are what we want to store
          */
-        const timestamp = Date.now();
+        let nonce = 0, hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.generateHash(timestamp, lastHash, data);
-        return new this(timestamp, lastHash, hash, data);
+
+        //try to find a hash that satisfy proof-of-work condition
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = Block.generateHash(timestamp, lastHash, data, nonce);
+        } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+
+        return new this(timestamp, lastHash, hash, data, nonce);
     }
 
-    static generateHash(timestamp, lastHash, data) {
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    static generateHash(timestamp, lastHash, data, nonce) {
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
     }
 
-    static blockHash(block){
-        const {timestamp, lastBlockHash, data} = block;
-        return Block.generateHash(timestamp, lastBlockHash, data);
+    static blockHash(block) {
+        const {timestamp, lastBlockHash, data, nonce} = block;
+        return Block.generateHash(timestamp, lastBlockHash, data, nonce);
     }
 }
 
