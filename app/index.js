4afbc7482af2b2bc400f8ddf0dc44009aeb5479d
amoazeni75 <amoazeni75@gmail.com>
2020-05-24 10:53:03 +0430

    -End point for adding a new block by miners implemented

--- app/index.js
+++ app/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000; //USER specify port or use default
 
 const app = express();
+app.use(bodyParser.json()); //this is a middleware which convert post request to json format
+
 const bc = new Blockchain();
 
 //end point for getting the chain
+//localhost:3000/blocks/
 app.get('/blocks', (req, res) => {
     res.json(bc.chain)
+});
+
+//end point adding block by miners
+//
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added with this data: ${block.toString()}`);
+
+    res.redirect('/blocks'); //to return updated chain
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on Port ${HTTP_PORT}`));
