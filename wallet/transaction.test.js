1cc3cff44dbd514168ae168b4831a58547edb5e7
amoazeni75 <amoazeni75@gmail.com>
2020-05-25 09:34:35 +0430

    -Verify Transaction and its test implemented

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -24,6 +24,16 @@ describe('Transaction', () => {
         expect(transaction.input.amount).toEqual(wallet.balance);
     });
 
+    it('validates a valid transaction', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true)
+    });
+
+
+    it('invalidates a corrupt transaction', () => {
+        transaction.outputs[0].amount = 5000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false)
+    });
+
     describe('transaction with the amount that exceeds the balance', () => {
         beforeEach(() => {
             amount = 5000;
