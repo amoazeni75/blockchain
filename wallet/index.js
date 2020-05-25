713c38b578415c30a9bc6c579e3dc6be98f6d191
amoazeni75 <amoazeni75@gmail.com>
2020-05-25 12:51:45 +0430

    -Wallet Test for creating and updating transaction completed

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const {INITIAL_BALANCE} = require('../config')
 const ChainUtil = require('../chain-util')
+const Transaction = require('./transaction')
 
 class Wallet {
     constructor() {
@@ -14,8 +15,23 @@ class Wallet {
         balance :${this.balance}`;
     }
 
-    sign(dataHash){
+    sign(dataHash) {
         return this.keyPair.sign(dataHash);
+    }
+
+    createTransaction(recipient, amount, transactionPool) {
+        if (amount > this.balance) {
+            console.log(`Amount: ${amount} exceeds the balance: ${this.balance}`);
+            return;
+        }
+        let transaction = transactionPool.existingTransaction(this.publicKey);
+        if (transaction) {
+            transaction.update(this, recipient, amount);
+        } else {
+            transaction = Transaction.newTransaction(this, recipient, amount);
+            transactionPool.updateOrAddTransaction(transaction);
+        }
+        return transaction;
     }
 }
 
