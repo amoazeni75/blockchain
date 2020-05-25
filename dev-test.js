43ef2cf271262235b74e7b00679acd39a661e347
amoazeni75 <amoazeni75@gmail.com>
2020-05-25 07:11:38 +0430

    -Wallet class created

--- dev-test.js
+++ dev-test.js
@@ -1,9 +1,16 @@
-const Block = require('./blockchain/block')
+// const Block = require('./blockchain/block')
+//
+// const block_instance = new Block('T', 'LH', 'H', 'D');
+// console.log(block_instance.toString());
+// console.log(Block.genesis().toString());
+//
+// // test mine block
+// const fooBlock = Block.mineBlock(Block.genesis(), 'foo data')
+// console.log(fooBlock.toString())
 
-const block_instance = new Block('T', 'LH', 'H', 'D');
-console.log(block_instance.toString());
-console.log(Block.genesis().toString());
+const Blockchain = require('./blockchain');
+const bc = new Blockchain();
 
-// test mine block
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo data')
-console.log(fooBlock.toString())
+for (let i = 0; i < 10; ++i) {
+    console.log(bc.addBlock(`data ${i}`).toString());
+}
